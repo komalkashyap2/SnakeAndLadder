e031bf54ea1f12025e46fa526fe148fc156e8e86
komalkashyap <komalkashyapmajri403@gmail.com>
2021-12-02 22:05:44 +0530

    [KOMAL KASHYAP] Added Random Player Roll The Die

---
+++ PlayerRollDie/PlayerRollDie/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerRollDie
+{
+      class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerRandomNum;
+
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Press any key to roll the dice.");
+
+                Console.ReadKey();
+
+                playerRandomNum = random.Next(1, 7);
+                Console.WriteLine("You rolled a " + playerRandomNum);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

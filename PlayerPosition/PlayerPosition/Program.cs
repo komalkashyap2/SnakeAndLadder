8a35d981c64411c7bee45c783774409ea7b7311b
komalkashyap <komalkashyapmajri403@gmail.com>
2021-12-02 21:55:23 +0530

    [KOMAL KASHYAP] Added Player position problem

---
+++ PlayerPosition/PlayerPosition/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerPosition
+{
+    internal class Program
+    {
+        public int position = 0;
+        
+
+        public void Player1()
+        {
+            Console.WriteLine("single player position : " + position);
+        }
+
+        static void Main(string[] args)
+        {
+            int position = 0;
+
+            Console.WriteLine("single player position : " + position);
+            Console.Read();
+        }
+    }
+}
